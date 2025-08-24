7d00b5a5cbdd719492d82d1d579055f4ca6bec50
siraj7s <sirajpanigrahi@gmail.com>
2025-08-24 13:34:59 +0530

    blog added

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_1P1xcP from "../../assets/my-journey.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmy-journey.md";
+import __ASTRO_IMAGE_IMPORT_MGbyL from "../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
+export default new Map([["../../assets/my-journey.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmy-journey.md", __ASTRO_IMAGE_IMPORT_1P1xcP], ["../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_MGbyL]]);
+		
